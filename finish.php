695dc7fb070ae1c27245ba812716d4c01c454600
bepeters <blake.peters@gmail.com>
2013-06-13 19:33:50 +0000

    fixed error

--- finish.php
+++ finish.php
@@ -18,12 +18,9 @@
  $Query = mysqli_query($connect, $sqli);
  $NumRows = mysqli_num_rows($Query);
  if($NumRows != 0)
-	{
-	while($Row = mysqli_fetch_assoc(Query))
-		{
-			$DB_Name = $Row['Username'];
-		{
-	}
+  {
+  	die("Username already taken, please choose a different Username");
+  }
 
  
 if($Username == "")
@@ -38,11 +35,7 @@ if($Username == "")
  {
  die("Your Passwords don't match, please try again!");
  }
- if($Username == $DB_Name)
- {
- die("Username already taken, please choose a different Username");
- }
-
+ 
 $sql="INSERT INTO Users (Username, Password) VALUES ('$Username', '$Password')";
 if(!mysqli_query($connect, $sql))
  {
